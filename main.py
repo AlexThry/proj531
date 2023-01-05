781c402f05295c23428209421b253d281599aead
Alexis Thierry <alexist103@gmail.com>
2023-01-05 13:40:21 +0100

    debut main

--- main.py
+++ main.py
@@ -4,3 +4,21 @@ import src.Class_Partie
 import src.Class_Quizz
 import src.Class_sauvegarde
 import src.Class_Utilisateur
+
+if __name__ == "__main__":
+  while True:
+    ########## MENU ##########
+    print()
+    print("########## MENU ########## \n")
+    print("1. Se Connecter")
+    print("2. Créer un compte")
+    print("3. Quitter \n")
+    rep = input("Faites votre choix \n")
+    if rep == "3":
+      break
+    elif rep == 1:
+      pass
+    elif rep == 2:
+      pass
+
+
--- src/Class_Utilisateur.py
+++ src/Class_Utilisateur.py
@@ -1,3 +1,7 @@
 class Utilisateur:
   def __init_(self):
     pass
+  def connextion(self):
+    login = input("Login : \n")
+    mdp = input("Mot de passe : \n")
+    adminKey = input("Clé administrateur : \n")

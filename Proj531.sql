7f225f5390edf379652552fbb88760343847476c
Alexis Thierry <alexist103@gmail.com>
2023-01-05 16:10:22 +0100

    creation database

--- Proj531.sql
+++ Proj531.sql
@@ -1,82 +1,65 @@
--- phpMyAdmin SQL Dump
--- version 5.2.0
--- https://www.phpmyadmin.net/
---
--- Hôte : localhost
--- Généré le : jeu. 05 jan. 2023 à 14:54
--- Version du serveur : 10.4.21-MariaDB
--- Version de PHP : 8.1.6
 
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-START TRANSACTION;
-SET time_zone = "+00:00";
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
 
 --
--- Base de données : `Proj531`
+-- Base de données : Proj531
 --
 
 -- --------------------------------------------------------
 
 --
--- Structure de la table `Historique`
+-- Structure de la table Historique
 --
 
-CREATE TABLE `Historique` (
-  `idHisto` int(11) NOT NULL,
-  `score` int(11) DEFAULT NULL,
-  `mode` int(11) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+CREATE TABLE Historique (
+  idHisto int(11) NOT NULL,
+  score int(11) DEFAULT NULL,
+  mode int(11) DEFAULT NULL
+) ;
 
 -- --------------------------------------------------------
 
 --
--- Structure de la table `Question`
+-- Structure de la table Question
 --
 
-CREATE TABLE `Question` (
-  `idQuestion` int(11) NOT NULL,
-  `question` text DEFAULT NULL,
-  `reponse1` text DEFAULT NULL,
-  `reponse2` text DEFAULT NULL,
-  `bonneReponse` text DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+CREATE TABLE Question (
+  idQuestion int(11) NOT NULL,
+  question text DEFAULT NULL,
+  reponse1 text DEFAULT NULL,
+  reponse2 text DEFAULT NULL,
+  bonneReponse text DEFAULT NULL
+) ;
 
 -- --------------------------------------------------------
 
 --
--- Structure de la table `quizz`
+-- Structure de la table quizz
 --
 
-CREATE TABLE `quizz` (
-  `idQuizz` int(11) NOT NULL,
-  `nom` varchar(50) DEFAULT NULL,
-  `theme` varchar(50) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+CREATE TABLE quizz (
+  idQuizz int(11) NOT NULL,
+  nom varchar(50) DEFAULT NULL,
+  theme varchar(50) DEFAULT NULL
+) ;
 
 -- --------------------------------------------------------
 
 --
--- Structure de la table `Utilisateur`
+-- Structure de la table Utilisateur
 --
 
-CREATE TABLE `Utilisateur` (
-  `idUtilisateur` int(11) NOT NULL,
-  `nom` varchar(50) DEFAULT NULL,
-  `mdp` varchar(50) DEFAULT NULL,
-  `isAdmin` tinyint(1) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+CREATE TABLE Utilisateur (
+  idUtilisateur int(11) NOT NULL,
+  nom varchar(50) DEFAULT NULL,
+  mdp varchar(50) DEFAULT NULL,
+  isAdmin tinyint(1) DEFAULT NULL
+) ;
 
 --
--- Déchargement des données de la table `Utilisateur`
+-- Déchargement des données de la table Utilisateur
 --
 
-INSERT INTO `Utilisateur` (`idUtilisateur`, `nom`, `mdp`, `isAdmin`) VALUES
+INSERT INTO Utilisateur (idUtilisateur, nom, mdp, isAdmin) VALUES
 (1, 'Alexis', '1234', 1),
 (2, 'Arthur', '1234', 1),
 (3, 'Andres', '1234', 1),
@@ -87,56 +70,56 @@ INSERT INTO `Utilisateur` (`idUtilisateur`, `nom`, `mdp`, `isAdmin`) VALUES
 --
 
 --
--- Index pour la table `Historique`
+-- Index pour la table Historique
 --
-ALTER TABLE `Historique`
-  ADD PRIMARY KEY (`idHisto`);
+ALTER TABLE Historique
+  ADD PRIMARY KEY (idHisto);
 
 --
--- Index pour la table `Question`
+-- Index pour la table Question
 --
-ALTER TABLE `Question`
-  ADD PRIMARY KEY (`idQuestion`);
+ALTER TABLE Question
+  ADD PRIMARY KEY (idQuestion);
 
 --
--- Index pour la table `quizz`
+-- Index pour la table quizz
 --
-ALTER TABLE `quizz`
-  ADD PRIMARY KEY (`idQuizz`);
+ALTER TABLE quizz
+  ADD PRIMARY KEY (idQuizz);
 
 --
--- Index pour la table `Utilisateur`
+-- Index pour la table Utilisateur
 --
-ALTER TABLE `Utilisateur`
-  ADD PRIMARY KEY (`idUtilisateur`);
+ALTER TABLE Utilisateur
+  ADD PRIMARY KEY (idUtilisateur);
 
 --
 -- AUTO_INCREMENT pour les tables déchargées
 --
 
 --
--- AUTO_INCREMENT pour la table `Historique`
+-- AUTO_INCREMENT pour la table Historique
 --
-ALTER TABLE `Historique`
-  MODIFY `idHisto` int(11) NOT NULL AUTO_INCREMENT;
+ALTER TABLE Historique
+  MODIFY idHisto int(11) NOT NULL AUTO_INCREMENT;
 
 --
--- AUTO_INCREMENT pour la table `Question`
+-- AUTO_INCREMENT pour la table Question
 --
-ALTER TABLE `Question`
-  MODIFY `idQuestion` int(11) NOT NULL AUTO_INCREMENT;
+ALTER TABLE Question
+  MODIFY idQuestion int(11) NOT NULL AUTO_INCREMENT;
 
 --
--- AUTO_INCREMENT pour la table `quizz`
+-- AUTO_INCREMENT pour la table quizz
 --
-ALTER TABLE `quizz`
-  MODIFY `idQuizz` int(11) NOT NULL AUTO_INCREMENT;
+ALTER TABLE quizz
+  MODIFY idQuizz int(11) NOT NULL AUTO_INCREMENT;
 
 --
--- AUTO_INCREMENT pour la table `Utilisateur`
+-- AUTO_INCREMENT pour la table Utilisateur
 --
-ALTER TABLE `Utilisateur`
-  MODIFY `idUtilisateur` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+ALTER TABLE Utilisateur
+  MODIFY idUtilisateur int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;

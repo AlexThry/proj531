8d17e1058a9ed540747417291e7cd892b3ac8b64
Alexis Thierry <alexist103@gmail.com>
2023-01-05 11:12:57 +0100

    ajout base de donnée

---
+++ assets/Proj531.sql
@@ -0,0 +1,134 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Hôte : localhost
+-- Généré le : jeu. 05 jan. 2023 à 11:11
+-- Version du serveur : 10.4.21-MariaDB
+-- Version de PHP : 8.1.6
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de données : `Proj531`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `Historique`
+--
+
+CREATE TABLE `Historique` (
+  `idHisto` int(11) NOT NULL,
+  `score` int(11) DEFAULT NULL,
+  `mode` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `Question`
+--
+
+CREATE TABLE `Question` (
+  `idQuestion` int(11) NOT NULL,
+  `question` text DEFAULT NULL,
+  `reponse1` text DEFAULT NULL,
+  `reponse2` text DEFAULT NULL,
+  `bonneReponse` text DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `quizz`
+--
+
+CREATE TABLE `quizz` (
+  `idQuizz` int(11) NOT NULL,
+  `nom` varchar(50) DEFAULT NULL,
+  `theme` varchar(50) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `Utilisateur`
+--
+
+CREATE TABLE `Utilisateur` (
+  `idUtilisateur` int(11) NOT NULL,
+  `nom` varchar(50) DEFAULT NULL,
+  `mdp` varchar(50) DEFAULT NULL,
+  `isAdmin` tinyint(1) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Index pour les tables déchargées
+--
+
+--
+-- Index pour la table `Historique`
+--
+ALTER TABLE `Historique`
+  ADD PRIMARY KEY (`idHisto`);
+
+--
+-- Index pour la table `Question`
+--
+ALTER TABLE `Question`
+  ADD PRIMARY KEY (`idQuestion`);
+
+--
+-- Index pour la table `quizz`
+--
+ALTER TABLE `quizz`
+  ADD PRIMARY KEY (`idQuizz`);
+
+--
+-- Index pour la table `Utilisateur`
+--
+ALTER TABLE `Utilisateur`
+  ADD PRIMARY KEY (`idUtilisateur`);
+
+--
+-- AUTO_INCREMENT pour les tables déchargées
+--
+
+--
+-- AUTO_INCREMENT pour la table `Historique`
+--
+ALTER TABLE `Historique`
+  MODIFY `idHisto` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT pour la table `Question`
+--
+ALTER TABLE `Question`
+  MODIFY `idQuestion` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT pour la table `quizz`
+--
+ALTER TABLE `quizz`
+  MODIFY `idQuizz` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT pour la table `Utilisateur`
+--
+ALTER TABLE `Utilisateur`
+  MODIFY `idUtilisateur` int(11) NOT NULL AUTO_INCREMENT;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;

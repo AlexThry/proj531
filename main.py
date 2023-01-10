eb3fd05516032b074c48dfff4f74bf07f9b46d5e
Arthur <fantache02@gmail.com>
2023-01-10 08:45:56 +0100

    lol

--- main.py
+++ main.py
@@ -6,37 +6,37 @@ from src.Class_Sauvegarde import *
 from src.Class_Utilisateur import *
 
 if __name__ == "__main__":
-	########## Ouverture database #########
-	conn = sql.connect("database.db")
-	curs = conn.cursor()
-	# print(curs.execute("""SELECT * FROM UTILISATEUR""").fetchall())
-	########## Création du Jeu ##########
+    ########## Ouverture database #########
+    conn = sql.connect("database.db")
+    curs = conn.cursor()
+    # print(curs.execute("""SELECT * FROM UTILISATEUR""").fetchall())
+    ########## Création du Jeu ##########
 
-	game = Jeu()
-	curs.execute("""
+    game = Jeu()
+    curs.execute("""
    CREATE TABLE `setQuestion` (
   `idQuestion` int(11) NOT NULL,
   `idQuizz` int(11) NOT NULL
 );
 	""")
-	########## MENU ##########
+    ########## MENU ##########
 
-	while True:
-		print(curs.execute("SELECT * FROM Utilisateur").fetchall())
-		print()
-		print("########## CONNEXION ########## \n")
-		print("1. Se Connecter")
-		print("2. Créer un compte")
-		print("3. Quitter \n")
-		rep = input("Faites votre choix \n")
-		if rep == "3":
-			curs.close()
-			break
-		elif rep == "2":
-			game.creer_compte(curs)
-		elif rep == "1":
-			game.connexion(curs)
-			if game.is_connected:
-				print("########## MENU ########## \n")
-				print("1. Jouer \n2. Historique \n")
-				rep = input("Faites votre choix \n")
+    while True:
+        print(curs.execute("SELECT * FROM Utilisateur").fetchall())
+        print()
+        print("########## CONNEXION ########## \n")
+        print("1. Se Connecter")
+        print("2. Créer un compte")
+        print("3. Quitter \n")
+        rep = input("Faites votre choix \n")
+        if rep == "3":
+            curs.close()
+            break
+        elif rep == "2":
+            game.creer_compte(curs)
+        elif rep == "1":
+            game.connexion(curs)
+            if game.is_connected:
+                print("########## MENU ########## \n")
+                print("1. Jouer \n2. Historique \n")
+                rep = input("Faites votre choix \n")
--- src/Class_jeu.py
+++ src/Class_jeu.py
@@ -13,24 +13,6 @@ class Jeu:
             if user[1] == login:
                 bon_mdp = user[2] == mdp
 
-<<<<<<< HEAD
-	def creer_compte(self, database):
-		login = input("Login : \n")
-		mdp = input("Mot de passe : \n")
-		is_Admin = input("Compte administrateur ? o/n : \n")
-		if is_Admin == "o":
-			admin_key = input("Clé administrateur : \n")
-			if admin_key == "1234":
-				database.execute(f"INSERT INTO VALUES (?,?,?,?)", (0, login, mdp, True))
-			else:
-				print("Vous ne pouvez pas être administrateur.")
-		else:
-			database.execute(f"INSERT INTO Utilisateur VALUES ({login}, {mdp}, false)")
-
-	def recuperer_sauvegarde(self):
-		pass
-			database.execute(f"INSERT INTO Utilisateur VALUES (?,?,?,?)", (0, login, mdp, False))
-=======
         if bon_mdp:
             self.is_connected = True
 
@@ -42,13 +24,12 @@ class Jeu:
             admin_key = input("Clé administrateur : \n")
             if admin_key == 1234:
                 database.execute(
-                    f"INSERT INTO Utilisateur VALUES ({login}, {mdp}, true)")
+                    f"INSERT INTO Utilisateur (nom,mdp,isadmin) VALUES ({login}, {mdp}, True)")
             else:
                 print("Vous ne pouvez pas être administrateur.")
         else:
             database.execute(
-                f"INSERT INTO Utilisateur VALUES ({login}, {mdp}, false)")
+                f"INSERT INTO Utilisateur (nom,mdp,isadmin) VALUES ({login}, {mdp}, False)")
 
     def recuperer_sauvegarde(self):
         pass
->>>>>>> 7a47928ea4c71c8ebf5d78a40ffe345cda86e1f4

21820d1b761530912ddeb6964b5445cc4a68c023
Arthur <fantache02@gmail.com>
2023-01-10 09:23:31 +0100

    ca flingue

--- src/Class_jeu.py
+++ src/Class_jeu.py
@@ -13,28 +13,6 @@ class Jeu:
             if user[1] == login:
                 bon_mdp = user[2] == mdp
 
-        def creer_compte(self, database):
-            login = input("Login : \n")
-            mdp = input("Mot de passe : \n")
-            is_Admin = input("Compte administrateur ? o/n : \n")
-            if is_Admin == "o":
-                admin_key = input("Clé administrateur : \n")
-                if admin_key == "1234":
-                    database.execute(
-                        f"INSERT INTO Utilisateur VALUES ({login}, {mdp}, true)")
-                else:
-                    print("Vous ne pouvez pas être administrateur.")
-            else:
-                database.execute(
-                    f"INSERT INTO Utilisateur VALUES ({login}, {mdp}, false)")
-
-        def recuperer_sauvegarde(self):
-            pass
-            database.execute(
-                f"INSERT INTO Utilisateur VALUES (?,?,?,?)", (0, login, mdp, False))
-        if bon_mdp:
-            self.is_connected = True
-
     def creer_compte(self, database):
         login = input("Login : \n")
         mdp = input("Mot de passe : \n")

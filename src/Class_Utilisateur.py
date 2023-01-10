b6a35f7b45df23b1c212c188efa32fa71efdda0d
Arthur <fantache02@gmail.com>
2023-01-10 10:41:00 +0100

    ca flingue

--- src/Class_Utilisateur.py
+++ src/Class_Utilisateur.py
@@ -2,3 +2,6 @@ class Utilisateur:
     def __init__(self, login, isadmin):
         self.login = login
         self.isadmin = isadmin
+
+    def historique(self, database, user):
+        user = database.execute(f'SELECT id_utilisateur FROM Utilisateur')

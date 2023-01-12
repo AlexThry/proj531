ec62ac387af49daae8b9d4fcd46760513881b379
Arthur <fantache02@gmail.com>
2023-01-12 10:01:16 +0100

    ca flingue

--- src/Class_Utilisateur.py
+++ src/Class_Utilisateur.py
@@ -3,8 +3,15 @@ class Utilisateur:
         self.login = login
         self.isadmin = isadmin
 
-    def historique(self, database, login):
+    def get_historique(self, login):
         id_user = database.execute(
             f'SELECT id_utilisateur FROM Utilisateur WHERE login = {login}')
-        historique = f'SELECT mode, score FROM HISTORIQUE WHERE id_user = {id_user}'
+        historique = database.execute(
+            f'SELECT mode, score FROM HISTORIQUE WHERE id_user = {id_user}')
         print(historique)
+
+    def edit_history(self, login, mode, score):
+        id_user = database.execute(
+            f'SELECT id_utilisateur FROM Utilisateur WHERE login = {login}')
+        insert_into_history = database.execute(
+            'INSERT INTO HISTORIQUE (idUtilisateur,score,mode) VALUES (?,?,?)', (id_user, mode, score))

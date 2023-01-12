0b696ef6f7bba21f1b1a42698dc82d94727d5556
Arthur <fantache02@gmail.com>
2023-01-12 14:14:37 +0100

    ca flingue

--- src/Class_Jeu.py
+++ src/Class_Jeu.py
@@ -3,59 +3,96 @@ import random
 
 
 class Jeu:
-	def __init__(self):
-		self.is_connected = False
-		self.is_admin = False
-		self.user = None
+    def __init__(self):
+        self.is_connected = False
+        self.is_admin = False
+        self.user = None
 
-	def get_user(self):
-		return self.user
+    def get_user(self) -> str:
+        """renvoie le nom d'utilisateur du joueur
 
-	def get_is_admin(self):
-		return self.is_admin
+        Returns:
+                str: le nom du joueur
+        """
+        return self.user
 
-	def get_is_connected(self) -> bool:
-		return self.is_connected
-	
-	def set_is_connected(self, bool) -> None:
-		self.is_connected = bool
+    def get_is_admin(self) -> bool:
+        """Renvoies True si le joueur est administrateur, False sinon
 
-	def connexion(self, curs, database):
-		bon_mdp = False
-		login = input("Login : \n")
-		if curs.execute(f"SELECT * FROM Utilisateur WHERE nom = '{login}'").fetchall():
-			mdp = input("Mot de passe : \n")
-			users = curs.execute(f"SELECT nom, mdp, isAdmin FROM Utilisateur WHERE nom = '{login}'").fetchall()
-			user = users[0]
-			bon_mdp = user[1] == mdp
-			if bon_mdp:
-				self.is_connected = True
-				self.user = login
-				database.set_user(login)
-				if user[2] == "TRUE":
-					self.is_admin = True
-			else:
-				print("Mauvais mot de passe")
-		else:
-			print("Cet utilisatuer n'éxiste pas. Tapez 4 pour voir la liste des utilisateurs.")
-	
-	def deconnexion(self):
-		self.is_connected = False
-		self.user = None
-		self.is_admin = False
+        Returns:
+                bool: True si admin False sinon
+        """
+        return self.is_admin
 
-	def get_admin_key(self):
-		path = os.path.join('assets', 'Admin_Key.txt')
-		with open(path, "r") as f:
-			res = f.read()
-			f.close()
-		return res
+    def get_is_connected(self) -> bool:
+        """Renvoies True si le joueur est administrateur, False sinon
 
-	def set_admin_key(self):
-		path = os.path.join("assets", "Admin_Key.txt")
-		if self.is_admin:
-			new_admin_key = input(
-				"Veuillez rentrer la nouvelle clé administrateur : \n")
-			with open(path, "w") as f:
-				f.write(new_admin_key)
-				f.close()
+        Returns:
+                bool: True si connecté False sinon
+        """
+        return self.is_connected
+
+    def set_is_connected(self, bool) -> None:
+        """permet de 
+
+        Args:
+                bool (_type_): _description_
+        """
+        self.is_connected = bool
+
+    def connexion(self, curs, database) -> None:
+        """permet à l'utilisateur de se connecter
+
+        Args:
+                curs (connexion): relie la database
+                database (Database): un objet qui permet de faire des requêtes à la base
+        """
+        bon_mdp = False
+        login = input("Login : \n")
+        if curs.execute(f"SELECT * FROM Utilisateur WHERE nom = '{login}'").fetchall():
+            mdp = input("Mot de passe : \n")
+            users = curs.execute(
+                f"SELECT nom, mdp, isAdmin FROM Utilisateur WHERE nom = '{login}'").fetchall()
+            user = users[0]
+            bon_mdp = user[1] == mdp
+            if bon_mdp:
+                self.is_connected = True
+                self.user = login
+                database.set_user(login)
+                if user[2] == "TRUE":
+                    self.is_admin = True
+            else:
+                print("Mauvais mot de passe")
+        else:
+            print(
+                "Cet utilisatuer n'éxiste pas. Tapez 4 pour voir la liste des utilisateurs.")
+
+    def deconnexion(self) -> None:
+        """permet à l'utilisateur de se déconnecter
+        """
+        self.is_connected = False
+        self.user = None
+        self.is_admin = False
+
+    def get_admin_key(self) -> str:
+        """renvoie la clé administrateur
+
+        Returns:
+                str: clé administrateur
+        """
+        path = os.path.join('assets', 'Admin_Key.txt')
+        with open(path, "r") as f:
+            res = f.read()
+            f.close()
+        return res
+
+    def set_admin_key(self) -> None:
+        """Permet de modifier la clé administrateur
+        """
+        path = os.path.join("assets", "Admin_Key.txt")
+        if self.is_admin:
+            new_admin_key = input(
+                "Veuillez rentrer la nouvelle clé administrateur : \n")
+            with open(path, "w") as f:
+                f.write(new_admin_key)
+                f.close()

9f60bdc710c222760f4c81616404546fc4995f47
cortesmc <carlosandres149@gmail.com>
2023-01-13 09:11:25 +0100

    interface quiz

--- src/Interface_choix_quizz.py
+++ src/Interface_choix_quizz.py
@@ -1,6 +1,12 @@
 from tkinter import *
+import sys
+sys.path.append("../")
+from src import Class_Jeu as JEU
 from src import Interface_login as IL
 from src import Class_Database as DT
+import sqlite3 as SQL
+import os
+
 
 
 class Interface_Choix_Quizz:
@@ -29,37 +35,36 @@ class Interface_Choix_Quizz:
 
 		label_titre = Label(self.logo, text="ZZuiQ", font=("Courrier", 80), bg=self.color, fg="White")
 		label_titre.grid(row=0, column=2, padx=10)
-        
+		
 		self.logo.place(relx=.5, rely=.1, anchor=CENTER)
 
-        
+		
 		self.btns = Frame(self.frame, bg=self.color)
-        
-        #Connexion BdD et recuperation
-        
-        conn=sqlite3.connect("database.db")
-        database = DT.Database()
-        cpt = 0
-        
-        while (database.recuperer_quizz(cpt,conn) != None):
-            text = database.recuperer_quizz(cpt,conn)
+		
+		conn = SQL.connect("database.db")
+		database = DT.Database()
+		cpt = 0
+		
+		print(database.recuperer_quizz(cpt,conn))
+				
+		while (database.recuperer_quizz(cpt,conn) == "[]"):
+			text = database.recuperer_quizz(cpt,conn)
 
-    		buttonTheme = Button(self.btns, text=text, font=(
-    			"Courrier", 18), bg="#FFCCFF", fg="#FF0080", width=30, command=self.login())
-    		buttonQuestion.grid(row=cpt+1, column=2, padx=10, pady=10)
-            
-            cpt +=1
-            
-		self.btns.place(relx=.5, rely=.5, anchor=CENTER)
-        
-        
+			buttonTheme = Button(self.btns, text=text, font=(
+				"Courrier", 18), bg="#FFCCFF", fg="#FF0080", width=30)
+			buttonTheme.grid(row=cpt+1, column=2, padx=10, pady=10)
+			
+			cpt +=1
+			self.btns.place(relx=.5, rely=.5, anchor=CENTER)
+			
+				
 
 
 	def afficher(self):
 		# afficher
 		self.window.mainloop()
-        
+		
 if __name__ == '__main__':
-    
-    window = Interface_Choix_Quizz()
-    window.afficher()
+	
+	window = Interface_Choix_Quizz()
+	window.afficher()
